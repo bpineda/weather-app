b2b25244b7ac636d269c944d8edd39854f81d560
Bernardo Pineda <bernardo.pineda@apple.com>
2021-12-18 14:39:32 -0800

    Passed city address to the service from the controller

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,7 +3,7 @@ class WeatherController < ApplicationController
     @address = params['address']
     unless @address.nil?
       weather = Weather.new
-      weather_data = weather.get_weather_data
+      weather_data = weather.get_weather_data @address
       puts weather_data.inspect
       @temperature = weather_data['main']['temp']
       @city = weather_data['name']

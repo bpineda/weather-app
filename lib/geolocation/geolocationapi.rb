2411281a9eedea9e5d5139e797c3d4aa53431ea4
Bernardo Pineda <bernardo.pineda@apple.com>
2021-12-18 14:08:37 -0800

    Passed API key and address string to the API

--- lib/geolocation/geolocationapi.rb
+++ lib/geolocation/geolocationapi.rb
@@ -7,7 +7,7 @@ class GeolocationApi
   API_KEY = Rails.application.credentials.geolocation_api_key
 
 
-  def self.get_city_by_address
+  def self.get_city_by_address(address_string)
     puts "Getting city by address"
     conn = Faraday.new(:url => BASE, :ssl => {:verify => false}) do |faraday|
       faraday.response :logger                  # log requests to $stdout
@@ -15,7 +15,7 @@ class GeolocationApi
     end
 
     response = conn.get do |req|
-      req.url "?access_key=9b3c1f513f78117e37b87a09410bf766&query=Tulsa"
+      req.url "?access_key=#{API_KEY}&query=#{address_string}"
     end
 
     response

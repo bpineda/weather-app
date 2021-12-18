e0891a7c5f672bf87cd92d61431fe2bf4cd041e7
Bernardo Pineda <bernardo.pineda@apple.com>
2021-12-18 14:04:55 -0800

    Added address, temperature, and city to the controller

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,5 +1,13 @@
 class WeatherController < ApplicationController
   def index
+    @address = params['address']
+    unless @address.nil?
+      weather = Weather.new
+      weather_data = weather.get_weather_data
+      puts weather_data.inspect
+      @temperature = weather_data['main']['temp']
+      @city = weather_data['name']
+    end
   end
 
   def get_by_city

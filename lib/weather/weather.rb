800183ad9b6e2ff31c7931a51ad57ecd7ab3f45e
Bernardo Pineda <bernardo.pineda@apple.com>
2021-12-18 14:39:08 -0800

    Passed city address and city name as argument

--- lib/weather/weather.rb
+++ lib/weather/weather.rb
@@ -5,11 +5,11 @@ require 'WeatherApi'
 class Weather
 
 
-  def get_weather_data
-    geolocation_raw_data = GeolocationApi.get_city_by_address
+  def get_weather_data(query_string)
+    geolocation_raw_data = GeolocationApi.get_city_by_address(query_string)
     geolocation_json = JSON.parse geolocation_raw_data.body
     city_name = geolocation_json['data'].first['name']
-    weather_raw_data = WeatherApi.get_weather_by_city
+    weather_raw_data = WeatherApi.get_weather_by_city city_name
     weather_json = JSON.parse weather_raw_data.body
     weather_json
   end

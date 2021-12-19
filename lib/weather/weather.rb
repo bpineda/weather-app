2e7c344e62b9360c291752684346da172da0770a
Bernardo Pineda <bernardo.pineda@apple.com>
2021-12-18 18:14:05 -0800

    Added geo api response; weather api response; zip code

--- lib/weather/weather.rb
+++ lib/weather/weather.rb
@@ -10,10 +10,14 @@ class Weather
     geolocation_json = JSON.parse geolocation_raw_data.body
 
     city_data = geolocation_json['results'].first['address_components'].select{|address| address['types'].include?("locality")}
+    zip_code_data = geolocation_json['results'].first['address_components'].select{|address| address['types'].include?("postal_code")}
     city_name = city_data.first
 
     weather_raw_data = WeatherApi.get_weather_by_city city_name['long_name']
     weather_json = JSON.parse weather_raw_data.body
+    weather_json['geo_response'] = geolocation_raw_data.body
+    weather_json['weather_response'] = weather_raw_data.body
+    weather_json['zip_code'] = zip_code_data.first['long_name']
     weather_json
   end
   

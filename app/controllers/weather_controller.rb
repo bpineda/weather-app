9a5c0556abed89b139bbf5c69d42c054c92f0fa1
Bernardo Pineda <bernardo.pineda@apple.com>
2021-12-18 18:16:45 -0800

    Added cached data; refresh cached data

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,12 +1,46 @@
 class WeatherController < ApplicationController
   def index
     @address = params['address']
+    @cached_result = false
     unless @address.nil?
-      weather = Weather.new
-      weather_data = weather.get_weather_data @address
-      puts weather_data.inspect
+
+      search_result = Search.find_or_create_by(query: @address)
+
+      if search_result.geo_response.nil?
+        # Fetch fresh results
+        weather = Weather.new
+        weather_data = weather.get_weather_data @address
+        search_result.geo_response = weather_data['geo_response']
+        search_result.weather_response = weather_data['weather_response']
+        search_result.zip_code = weather_data['zip_code']
+        search_result.save
+
+      else
+
+        @cached_result = true
+        elapsed_time = Time.now - search_result.updated_at
+        if elapsed_time > 30
+          # Fetch results again
+          @cached_result = false
+          weather = Weather.new
+          weather_data = weather.get_weather_data @address
+          search_result.geo_response = weather_data['geo_response']
+          search_result.weather_response = weather_data['weather_response']
+          search_result.zip_code = weather_data['zip_code']
+          search_result.save
+          search_result.touch
+
+        end
+      end
+
+      weather_data = JSON.parse search_result['weather_response']
       @temperature = weather_data['main']['temp']
+      @max = weather_data['main']['temp_max']
+      @low = weather_data['main']['temp_min']
+      @pressure = weather_data['main']['pressure']
+      @humidity = weather_data['main']['humidity']
       @city = weather_data['name']
+      @zip_code = search_result.zip_code
     end
   end
 

f1a406355bbef0c5d27746b734491bd33e09531b
Bernardo Pineda <bernardo.pineda@apple.com>
2021-12-18 19:01:08 -0800

    Added route test for empty search

--- spec/requests/weather_spec.rb
+++ spec/requests/weather_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe "Weathers", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+  describe "GET /search" do
+    it "returns http success" do
+      get "/search"
+      expect(response).to have_http_status(:success)
+    end
+  end
 
 end
--- spec/views/weather/index.html.erb_spec.rb
+++ spec/views/weather/index.html.erb_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "weather/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "weather/index", type: :view do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it "displays correct labels" do
+      render :template => "weather/index"
+
+      expect(rendered).to match /WeatherApp/
+      expect(rendered).to match /Enter an address/
+      expect(rendered).to_not match /City/
+  end
 end

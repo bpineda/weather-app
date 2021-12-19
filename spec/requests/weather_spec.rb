dc4e85490125a2aa31a769004bd4a3ec997a8961
Bernardo Pineda <bernardo.pineda@apple.com>
2021-12-18 19:51:24 -0800

    Implemented VCR mocked API calls

--- spec/requests/weather_spec.rb
+++ spec/requests/weather_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.filter_sensitive_data('<G_API_KEY>') { Rails.application.credentials.geolocation_api_key }
+  config.filter_sensitive_data('<WEATHER_API_KEY>') { Rails.application.credentials.weather_api_key }
+  config.hook_into :faraday
+end
+
 RSpec.describe "Weathers", type: :request do
   describe "GET /index" do
     it "returns http success" do
@@ -7,7 +14,7 @@ RSpec.describe "Weathers", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
-  
+
   describe "GET /search" do
     it "returns http success" do
       get "/search"
@@ -17,7 +24,12 @@ RSpec.describe "Weathers", type: :request do
 
   describe "GET /search" do
     it "returns http success" do
-      get "/search?address=1+Apple+Park+Way+Cupertino%2C+California%2C+95014+United+States&commit=SEARCH"
+
+      VCR.use_cassette("geolocalization_call") do
+        get "/search?address=1+Apple+Park+Way+Cupertino%2C+California%2C+95014+United+States&commit=SEARCH"
+      end
+
+      # get "/search?address=1+Apple+Park+Way+Cupertino%2C+California%2C+95014+United+States&commit=SEARCH"
       expect(response).to have_http_status(:success)
     end
   end
